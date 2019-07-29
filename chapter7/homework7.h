08567df6545a2bca1f9a6a0e64d864021b62ed26
wmhwmh521 <51085721+wmhwmh521@users.noreply.github.com>
2019-07-29 13:18:55 +0800

    Rename Sales_data.h to homework7.h

--- chapter7/homework7.h
+++ chapter7/homework7.h
@@ -10,16 +10,16 @@ struct Sales_data {
 	unsigned units_sold = 0;
 	double revenue = 0.0;
 
-	string isbn() const {    //const意为不改变this中的内容
+	string isbn() const {    //const鎰忎负涓嶆敼鍙榯his涓殑鍐呭
 		return this->bookNo;
 	}
-	Sales_data& combine(const Sales_data&); //声明函数
+	Sales_data& combine(const Sales_data&); //澹版槑鍑芥暟
 	istream& read(istream& is, Sales_data& item);
 	ostream& print(ostream& os, Sales_data& item);
 	Sales_data add(const Sales_data& lhs, const Sales_data& rhs);
 };
 
-Sales_data& Sales_data::combine(const Sales_data& rhs) {  //重写函数
+Sales_data& Sales_data::combine(const Sales_data& rhs) {  //閲嶅啓鍑芥暟
 	units_sold += rhs.units_sold;
 	revenue += rhs.revenue;
 	return *this;
